fb9e4687a8477b583a4124efcf856e3e7db7d502
ipanteev <blackiv@gmail.com>
2023-12-10 19:59:00 +0700

    Исправления по замечаниям

--- WorkWithObjects/Objects.js
+++ WorkWithObjects/Objects.js
@@ -1,38 +1,66 @@
 (function () {
-    const countryArray = [
+    function findCountryWithMaxCitiesCount() {
+        const maxCitiesCount = countries.reduce((maxCities, item) => Math.max(maxCities, item.cities.length), 0);
+        return countries
+            .filter(country => country.cities.length === maxCitiesCount)
+            .map(country => country.name);
+    }
+
+    function getPopulationByCountries(){
+        const populationByCountries = {};
+
+        countries.forEach(country => {
+            populationByCountries[country.name] = country.cities.reduce((population, city) => population + city.population, 0);
+        });
+
+        return populationByCountries;
+    }
+
+    const countries = [
         {
             name: "Россия",
-            cityList: [{name: "Москва", population: 12},
-                {name: "Санкт-Петербург", population: 7},
-                {name: "Новосибирск", population: 2.5}]
+            cities: [
+                {
+                    name: "Москва",
+                    population: 12},
+                {
+                    name: "Санкт-Петербург",
+                    population: 7},
+                {
+                    name: "Новосибирск",
+                    population: 2.5}
+            ]
         },
         {
             name: "Казахстан",
-            cityList: [{name: "Астана", population: 1},
-                {name: "Алма-аты", population: 1.8}]
+            cities: [
+                {
+                    name: "Астана",
+                    population: 1},
+                {
+                    name: "Алма-аты",
+                    population: 1.8}
+            ]
         },
         {
             name: "Китай",
-            cityList: [{name: "Пекин", population: 21.8},
-                {name: "Макао", population: 0.63}]
+            cities: [
+                {
+                    name: "Пекин",
+                    population: 21.8},
+                {
+                    name: "Макао",
+                    population: 0.63}
+            ]
         }
     ];
 
-    const maxCitiesCount = countryArray.reduce((maxCities, item) => Math.max(maxCities, item.cityList.length), 0);
-    console.log(`Максимальное количество городов ${maxCitiesCount}`);
+    console.log(`Страны с максимальным количеством городов [${findCountryWithMaxCitiesCount()}]`);
 
-    const maxCitiesCountries = countryArray
-        .filter(country => country.cityList.length === maxCitiesCount)
-        .map(country => country.name);
-    console.log(`Страны с максимальным количеством городов [${maxCitiesCountries}]`);
+    console.log("Информация по странам");
+    const populationByCountries = getPopulationByCountries()
 
-    const countriesInformation = countryArray.reduce((information, country) => {
-        information[country.name] = country.cityList.reduce((population, city) => population + city.population, 0);
-        return information;
-    }, {});
-    console.log(`Информация по странам`);
-
-    for (let country in countriesInformation) {
-        console.log(`Население страны ${country} составляет ${countriesInformation[country]}`);
+    for (let country in populationByCountries) {
+        console.log(`Население страны ${country} составляет ${populationByCountries[country]}`);
     }
 })();
