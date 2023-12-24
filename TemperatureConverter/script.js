5b3cf2a8c38cb319ec46951c158f6e41b84a1d35
ipanteev <blackiv@gmail.com>
2023-12-24 17:03:15 +0700

    Исправления по замечаниям

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const convertForm = document.getElementById("convert-form");
     const celsiusDegreesField = document.getElementById("celsius-degrees-field");
-    const kelvinDegreesText = document.getElementById("kelvin-degrees-text");
-    const fahrenheitDegreesText = document.getElementById("fahrenheit-degrees-text");
-
-    celsiusDegreesField.addEventListener("change", function () {
-        convertForm.requestSubmit();
-    });
+    const kelvinDegreesTextField = document.getElementById("kelvin-degrees-text");
+    const fahrenheitDegreesTextField = document.getElementById("fahrenheit-degrees-text");
 
     celsiusDegreesField.addEventListener("input", function () {
         convertForm.requestSubmit();
@@ -16,13 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
         e.preventDefault();
 
         if (celsiusDegreesField.value === "") {
-            kelvinDegreesText.textContent = "";
-            fahrenheitDegreesText.textContent = "";
-        }
-        else{
+            kelvinDegreesTextField.textContent = "";
+            fahrenheitDegreesTextField.textContent = "";
+        } else {
             const celsiusDegrees = parseFloat(celsiusDegreesField.value);
-            kelvinDegreesText.textContent = (celsiusDegrees + 273.15).toFixed(2);
-            fahrenheitDegreesText.textContent = ((celsiusDegrees - 32) * 5 / 9).toFixed(2);
+            kelvinDegreesTextField.textContent = (celsiusDegrees + 273.15).toFixed(2);
+            fahrenheitDegreesTextField.textContent = ((celsiusDegrees - 32) * 5 / 9).toFixed(2);
         }
 
     });
