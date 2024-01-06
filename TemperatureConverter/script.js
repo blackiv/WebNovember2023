7785270e32027879d3efd37a8bce5a1bdd6c7bca
ipanteev <blackiv@gmail.com>
2024-01-06 18:05:46 +0700

    Исправления по замечаниям

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     const convertForm = document.getElementById("convert-form");
-    const celsiusDegreesField = document.getElementById("celsius-degrees-field");
-    const kelvinDegreesTextField = document.getElementById("kelvin-degrees-text");
-    const fahrenheitDegreesTextField = document.getElementById("fahrenheit-degrees-text");
+    const celsiusDegreesTextField = document.getElementById("celsius-degrees-field");
+    const kelvinDegreesResultField = document.getElementById("kelvin-degrees-text");
+    const fahrenheitDegreesResultField = document.getElementById("fahrenheit-degrees-text");
 
-    celsiusDegreesField.addEventListener("input", function () {
+    celsiusDegreesTextField.addEventListener("input", function () {
         convertForm.requestSubmit();
     });
 
     convertForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
-        if (celsiusDegreesField.value === "") {
-            kelvinDegreesTextField.textContent = "";
-            fahrenheitDegreesTextField.textContent = "";
+        if (celsiusDegreesTextField.value === "") {
+            kelvinDegreesResultField.textContent = "";
+            fahrenheitDegreesResultField.textContent = "";
         } else {
-            const celsiusDegrees = parseFloat(celsiusDegreesField.value);
-            kelvinDegreesTextField.textContent = (celsiusDegrees + 273.15).toFixed(2);
-            fahrenheitDegreesTextField.textContent = ((celsiusDegrees - 32) * 5 / 9).toFixed(2);
+            const celsiusDegrees = parseFloat(celsiusDegreesTextField.value);
+            kelvinDegreesResultField.textContent = (celsiusDegrees + 273.15).toFixed(2);
+            fahrenheitDegreesResultField.textContent = ((celsiusDegrees * 5 / 9) + 32).toFixed(2);
         }
-
     });
+
     convertForm.requestSubmit();
 });
